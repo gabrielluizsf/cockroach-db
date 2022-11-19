f7a4137058023dbb36430e6f5f610d3c13aab0ed
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-11-19 00:33:59 -0300

    initing server

---
+++ server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"log"
+	//"os"
+	"github.com/GabrielLuizSF/cockroach-db/server/routes"
+	"github.com/gin-gonic/gin"
+
+)
+
+type Server struct {
+	port   string
+	server *gin.Engine
+}
+
+func NewServer() Server {
+	localPort := "5000";
+	//serverPort := os.Getenv("PORT");
+	return Server{
+		port:  localPort,
+		server: gin.Default(),
+	}
+
+}
+
+func (s *Server) Run() {
+	router := routes.ConfigRoutes(s.server)
+
+	log.Printf("Server running at port: %v", s.port)
+	log.Fatal(router.Run(":" + s.port))
+}
